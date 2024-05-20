f9c95f0237186f99339a768d74ec574292dfd170
Ditisha <ditisha03@gmail.com>
2024-05-21 02:26:10 +0600

    Updating my work2

--- app/src/main/java/com/example/ditisha/MainActivity.java
+++ app/src/main/java/com/example/ditisha/MainActivity.java
@@ -7,12 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.setValue("Hello");
 
         ImageView sohoj = (ImageView) findViewById(R.id.sohoj);
         sohoj.setOnClickListener(new View.OnClickListener() {
